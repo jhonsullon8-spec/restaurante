1380a62040d9c92cdad38a08fb3640e50dd19651
Lenner <jhonsullon8@gmail.com>
2025-11-22 14:31:53 -0500

    Eliminar node_modules del repositorio

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,20 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "restaurante"
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
-db.connect(err => {
+db.connect((err) => {
   if (err) {
-    console.error("❌ Error al conectar con MySQL:", err);
-  } else {
-    console.log("✅ Conectado a MySQL");
+    console.error("❌ Error al conectar a la DB:", err);
+    return;
   }
+  console.log("✅ Conexión MySQL Railway exitosa");
 });
 
 module.exports = db;
